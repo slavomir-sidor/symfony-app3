7781313e4720fd17bb13da3728ebbab8919f3f1e
Slavomir Kuzma <slavomir.kuzma@gmail.com>
2015-12-07 00:37:44 +0100

    XML Updates.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,10 +1,10 @@
 <?php
-
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+
     public function registerBundles()
     {
         $bundles = array(
@@ -15,16 +15,19 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new AppBundle\AppBundle()
         );
-
-        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+        
+        if (in_array($this->getEnvironment(), array(
+            'dev',
+            'test'
+        ), true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
-
+        
         return $bundles;
     }
 
@@ -35,16 +38,26 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
     }
 
     public function getLogDir()
     {
-        return dirname(__DIR__).'/var/logs';
+        return dirname(__DIR__) . '/var/logs';
     }
 
+    /**
+     *
+     * @param LoaderInterface $loader            
+     */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $environment = $this->getEnvironment();
+        $config = __DIR__ . sprintf('/config/%s/config.xml', $environment);
+        if (! file_exists($config)) {
+            
+            $config = __DIR__ . sprintf('/config/config.xml');
+        }
+        $loader->load($config);
     }
 }
